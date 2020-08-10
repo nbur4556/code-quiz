7a78a6365736695b2650110677109415834aee3e
nbur4556 <nbur4556@gmail.com>
2020-08-10 17:14:08 -0500

    created multiple choice questions constructor to generate multiple choice objects

--- assets/scripts/mainscript.js
+++ assets/scripts/mainscript.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+function MultipleChoiceQuestion(question, answers, correctAnswerIndex) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswerIndex = correctAnswerIndex;
+}
+
+window.onload = function () {
+    let questionArray = new Array();
+
+    questionArray[0] = new MultipleChoiceQuestion(
+        'How many licks does it take to get to the center of a tootsie pop?',
+        [
+            '1',
+            '2',
+            '3'
+        ],
+        1
+    );
+    questionArray[1] = new MultipleChoiceQuestion(
+        'Why did the chicken cross the road?',
+        [
+            'To get to the other side.',
+            'Because it wanted to.',
+            'Just for fun!'
+        ],
+        3
+    );
+
+    console.log(questionArray);
+
+    return questionArray;
+}
